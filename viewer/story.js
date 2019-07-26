bb0b3b2a83f0dffb42b5376474c66312f26e7425
Jose.Glas <48224956+JoseGlasOLX@users.noreply.github.com>
2019-07-26 16:22:49 -0300

    1er comint
    upload files

---
+++ viewer/story.js
@@ -0,0 +1,136 @@
+var story = {
+"docName": "landing-alianzas-look-and-feel",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": true,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "look_and_field_1_copy.png",
+"image2x": "look_and_field_1_copy@2x.png",
+"width": 1440,
+"height": 2570,
+"title": "Look and field 1 Copy",
+'layout' : {
+	"offset": 150,
+	"totalWidth": 1140,
+	"numberOfColumns": 12,
+	"columnWidth": 67.5,
+	"gutterWidth": 30
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1440,
+		"height": 100,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "look_and_field_1_copy-0.png",
+		"image2x": "look_and_field_1_copy-0@2x.png"
+	}
+],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 1,
+"image": "look_and_field_1.png",
+"image2x": "look_and_field_1@2x.png",
+"width": 1440,
+"height": 2470,
+"title": "Look and field 1",
+'layout' : {
+	"offset": 150,
+	"totalWidth": 1140,
+	"numberOfColumns": 12,
+	"columnWidth": 67.5,
+	"gutterWidth": 30
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 2,
+"image": "wireframe_6.png",
+"image2x": "wireframe_6@2x.png",
+"width": 1440,
+"height": 1747,
+"title": "Wireframe 6",
+'layout' : {
+	"offset": 150,
+	"totalWidth": 1140,
+	"numberOfColumns": 12,
+	"columnWidth": 67.5,
+	"gutterWidth": 30
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1440,
+		"height": 100,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "wireframe_6-0.png",
+		"image2x": "wireframe_6-0@2x.png",
+		"shadow": "0px 0px 10px 0 #00000040 ",
+		"shadowX": 10
+	}
+],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 3,
+"image": "wire_frame_kit_copy.png",
+"image2x": "wire_frame_kit_copy@2x.png",
+"width": 1440,
+"height": 10352,
+"title": "Wire Frame Kit Copy",
+'layout' : {
+	"offset": 150,
+	"totalWidth": 1140,
+	"numberOfColumns": 12,
+	"columnWidth": 67.5,
+	"gutterWidth": 30
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+   ]
+,"resolutions": [2],
+"zoomEnabled": true,
+"title": "Landing-Alianzas-Look_and_Feel",
+"layersExist": true,
+"centerContent":  false,
+"totalImages": 6,
+"highlightLinks": false
+}
